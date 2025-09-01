f110dd365fa7cc4940c85cd362ce169412adf4ee
GrayThomsen <emilthomsen98@live.dk>
2025-09-01 16:48:35 +0200

    OptimizedJS

--- feed.js
+++ feed.js
@@ -1,48 +1,57 @@
-const  pip1 = {
-    name: "Dayan",
-    message: "Long text about something",
-    timestamp: 23-4-2024,
-}
-const  pip2 = {
-    name: "Signe",
-    message: "Comment on an image",
-    timestamp: 22-4-2024,
-}
-const  pip3 = {
-    name: "Pavla",
-    message: "Medium text about something",
-    timestamp: 20-4-2024,
-}
-const  pip4 = {
-    name: "Emil",
-    message: "Short text about something",
-    timestamp: 26-4-2024,
-}
+document.getElementById("workField").addEventListener("submit", (event) => {
+  event.preventDefault(); // stopper standard opførslen, hvor browseren reloader siden
 
-const pipFeed = [pip1, pip2, pip3, pip4]
+  const name = document.getElementById("writeTextName").value;
+  //   const timestamp = document.getElementById("timestamp").value;
+  const message = document.getElementById("writeTextMessage").value;
 
-pipFeed.forEach((pip) => {
-    // html template som hedder student, som definerer en studerende i html
-    let pipHtml = document.getElementById("pips"); 
-     console.log(pipHtml);
-    
-    // opretter en kopi fordi jeg skal have en templates indhold per student
-    let clon = pipHtml.content.cloneNode(true);
-    // console.log(clon);
-    
-    // udvælger navnet, email og phone
-    let pName = clon.querySelector(".userName");
-    let pTime = clon.querySelector(".timeStamp");
-    let pMessage = clon.querySelector(".userMessage");
+  if (name === "") {
+    alert("Der mangler et navn");
+  }
+  if (message === "") {
+    alert("Der mangler tekst");
+  }
 
-    // Sætter jeg den studerendes værdier ind i klonen af templaten
-    pName.innerText = pip.name;
-    pTime.innerText = pip.timestamp;
-    pMessage.innerText = pip.message;
+  if (name !== "" && message !== "") {
+    addPipsToDOM(name, message);
+  }
+});
 
-    // indsætter vi templaten i html dokumentet (så brugeren kan se den)
-    document.getElementById("yourPipFeed").appendChild(clon);
-    
-    // console.log(element);
-    // alert("Now calling " + element.tlf + " to speak with " + element.name)
-})
+const pip1 = {
+  name: "Dayan",
+  message: "Long text about something",
+  timestamp: "23-04-2024",
+};
+const pip2 = {
+  name: "Signe",
+  message: "Comment on an image",
+  timestamp: "22-04-2024",
+};
+const pip3 = {
+  name: "Pavla",
+  message: "Medium text about something",
+  timestamp: "20-04-2024",
+};
+const pip4 = {
+  name: "Emil",
+  message: "Short text about something",
+  timestamp: "26-04-2024",
+};
+
+const pipFeed = [pip1, pip2, pip3, pip4];
+
+function addPipsToDOM(name, message, timestamp) {
+  let pipHtml = document.getElementById("pips");
+
+  // opretter en kopi fordi jeg skal have en templates indhold per pip
+  let clon = pipHtml.content.cloneNode(true);
+  // console.log(clon);
+
+  // Sætter den studerendes værdier ind i klonen af templaten
+  clon.querySelector(".userName").innerText = name;
+  clon.querySelector(".timeStamp").innerText = timestamp;
+  clon.querySelector(".userMessage").innerText = message;
+
+  // indsætter templaten i html dokumentet (så brugeren kan se den)
+  document.getElementById("yourPipFeed").appendChild(clon);
+}
